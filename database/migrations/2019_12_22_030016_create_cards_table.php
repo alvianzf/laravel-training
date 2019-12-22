e9ce1c26b184aa5cd78beef379a1095487446455
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-12-22 10:06:12 +0700

    add controllers and models

---
+++ database/migrations/2019_12_22_030016_create_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('list_id')->unsigned();
+            $table->integer('order');
+            $table->string('task');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
---
+++ database/migrations/2019_12_22_030145_create_login_tokens_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_tokens');
+    }
+}
